2e4291f6e4debac3607ede80a89cd0969c93c15c
dev-deepak-prajapati <dev.deepak.prajapati@gmail.com>
2026-09-18 19:36:40 +0530

    Circular Linked List Operation Perform

--- src/circularlinkedlist/CircularSinglyLL.java
+++ src/circularlinkedlist/CircularSinglyLL.java
@@ -115,23 +115,68 @@ public class CircularSinglyLL {
 //        }
     }
 
+    public void add(int index, int data) {
+        if (index < 0 || index > length) {
+            System.out.println("Invalid index " + index);
+            System.out.println("index must be 0 to size-1.");
+            return;
+        }
+
+        if (index == 0) {
+            addFirst(data);
+            return;
+        }
+        if (index == length) {
+            addLast(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int remove(int index) {
+        if (head == null) {
+            System.out.println("LL is empty.");
+            return Integer.MIN_VALUE;
+        }
+        if (index < 0 || index > length - 1) {
+            System.out.println("Invalid index " + index);
+            System.out.println("index must be 0 to size-1.");
+            return Integer.MIN_VALUE;
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        if (index == length - 1) {
+            return removeLast();
+        }
+
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        int val = temp.next.data;
+        temp.next = temp.next.next;
+        return val;
+    }
+
     public static void main(String[] args) {
         CircularSinglyLL cll = new CircularSinglyLL();
         cll.addFirst(1);
-        cll.addFirst(2);
-        cll.addFirst(3);
         cll.addFirst(4);
         cll.addLast(5);
-        cll.addLast(6);
         cll.addLast(7);
         cll.addLast(8);
+        cll.add(4, 5);
         cll.print();
-        cll.size();
+        System.out.println(cll.remove(2));
+        cll.print();
 
-        System.out.println(cll.removeFirst());
-        System.out.println(cll.removeLast());
-        cll.print();
-        cll.size();
     }
 
 }
